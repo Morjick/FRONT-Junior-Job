7bdaad9b67aea7bd0401c385a6c43206c41d9654
Noobjke <sadko67@inbox.ru>
2023-02-13 16:56:15 +0300

    добавил шапку(частично)

--- src/app/layout/default.vue
+++ src/app/layout/default.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <router-link to="/">General</router-link>
-    <router-link to="/about">About Us</router-link>
-    <router-view />
+  <div class="pages">
+    <div class="pages-header">
+      <div class="burger-menu"></div>
+      <span class="page-logo">Junior Job</span>
+    </div>
+    <div class="pages-body">
+      <router-view />
+    </div>
   </div>
 </template>
 
