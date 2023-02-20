9dbd16549026849d573c4495ef0b62f15742ba94
Morjick <matvey200171@gmail.com>
2023-02-21 01:31:11 +0300

    fix bandle

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,7 +3,7 @@ import { BuildOptions } from './types/config';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import { VueLoaderPlugin } from 'vue-loader'
-import { BundleAnalyzerPlugin  } from 'webpack-bundle-analyzer'
+// import { BundleAnalyzerPlugin  } from 'webpack-bundle-analyzer'
 
 export function buildPlugins (options: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
@@ -21,8 +21,8 @@ export function buildPlugins (options: BuildOptions): webpack.WebpackPluginInsta
       chunkFilename: 'css/[name].[contenthash:8].css'
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
-    })
+    // new BundleAnalyzerPlugin({
+    //   openAnalyzer: false
+    // })
   ]
 }
