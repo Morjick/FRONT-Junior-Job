e0be1b71c67a5b69dc24c84bd4e14bac8be17eca
Morjick <matvey200171@gmail.com>
2023-03-04 17:03:26 +0300

    add admin

---
+++ src/app/layout/admin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="admin-layout">
+    <div class="admin-sidebar"></div>
+    <div class="admin-window">
+      <div class="admin-window-header"></div>
+      <div class="admin-window-body"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'admin',
+  data: () => ({}),
+  components: {},
+  props: {},
+  computed: {},
+  methods: {},
+  mounted () {}
+})
+</script>
