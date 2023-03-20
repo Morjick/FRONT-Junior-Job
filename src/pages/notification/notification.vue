0d5779be9e79c4cad7c9f3f86295330d1f51986d
Noobjke <sadko67@inbox.ru>
2023-03-21 00:23:19 +0300

    notification

---
+++ src/pages/notification/notification.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="notification"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'NotificationPage',
+  data: () => ({}),
+  components: {},
+  props: {},
+  computed: {},
+  methods: {},
+  mounted () {},
+})
+</script>
