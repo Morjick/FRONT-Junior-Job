0c42886623efe37d82b63189f5f33d997d73fef3
Ilya Karpov <karpov.ilya05@mail.ru>
2023-03-30 21:54:41 +0300

    Error handler

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,37 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import CKEditor from '@ckeditor/ckeditor5-vue'
+import { type IError } from 'widgets/interface/error'
 import 'shared/assets/styles/main.scss'
 
 const app = createApp({
   render: () => h(App),
 })
 
-app.config.errorHandler = function (err: any, vm: any, info: any) {
-  if (err.search(/\d/g) !== -1) {
-    const errCode = Number(err.match(/\d/g).join(''))
-
+try {
+  app.config.errorHandler = (err: IError | any): void => {
+    const errCode = Number(err.message.match(/\d/g)?.join(''))
     if (errCode >= 500 && errCode <= 600) {
       void router.push('/server-error')
     } else if (errCode >= 305 && errCode <= 401) {
       void router.push('')
     } else if (errCode === 403) {
       void router.push('/auth')
+    } else {
+      throw err
     }
-  } else {
-    console.error(err)
   }
+} catch (error: IError | any) {
+  throw error.message
 }
 
 axios.defaults.baseURL = ''
+axios.interceptors.response.use(
+  (res: any) => res,
+  (err: IError | any) => {
+    throw new Error(err)
+  }
+)
 
 app.use(router)
 app.use(store)
---
+++ src/widgets/interface/error.ts
@@ -0,0 +1,7 @@
+interface IError {
+  name: string
+  message: string
+  stack: string
+}
+
+export { type IError }
