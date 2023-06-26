a93e896b1c53a2e20eee08b53ea710917df72d4e
Admin <admin@Admins-MacBook-Air.local>
2023-06-26 18:58:54 +0300

    settings

--- src/pages/settings/settings.vue
+++ src/pages/settings/settings.vue
@@ -2,7 +2,7 @@
    <div class="general" @click="show=!show"  >
         <h1 class="title">Общее</h1>
         <div class="arrow_icon">
-            <img class="arrow"   src="~/shared/assets/images/arrow_back_ios.png">  
+            <img class="arrow" :class="{'turn-arrow': show}"  src="~/shared/assets/images/arrow_back_ios.png">  
         </div>   
    </div>
    <div class="bodyGeneral" v-show="show">
@@ -20,44 +20,56 @@
         <div class="notifications" @click="box=!box" >
             <h2 class="title_1">Уведомления</h2>
             <div class="arrow_icon">
-                <img class="arrow" src="~/shared/assets/images/arrow_back_ios.png">   
+                <img class="arrow" :class="{'turn-arrow':box}" src="~/shared/assets/images/arrow_back_ping.png">   
             </div>
         </div>
         <div class="pyncts"  v-show="box">
-            <div class="box_one">
-                <UiCheckbox  class="checkOne"  name="text1"/>
-                    <label class="pynctsTextOne" for="text1">Вас выбрали исполнителем/<br>На вашу вакансию откликнулись</label>
+            <div class="box_one" > 
+                <UiCheckbox  class="checkOne"  name="text1"/> 
+                    <label class="pynctsTextOne" for="text1" style="position: center;">
+                         Вас выбрали исполнителем/<br>На вашу вакансию откликнулись
+                    </label>
+             
+           
             </div>
             <div class="box_two">
-                <UiCheckbox  class="checkTwo" name="text2"/>
-                    <label class="pynctsTextTwo" for="text2">Вакансия, на которую вы<br>откликались, закрыта<br>работодателем</label>
+                    <UiCheckbox  class="checkTwo" name="text2"/>
+                    <label class="pynctsTextTwo" for="text2">
+                        Вакансия, на которую вы<br>откликались, закрыта<br>работодателем
+                    </label>
             </div>
-            <div class="box_three">
-                <UiCheckbox type="checkbox" class="checkThree" name="text3"/>
-                    <label class="pynctsTextThree" for="text3">Новые сообщения</label>
+            <div class="box_three" >
+                <UiCheckbox type="checkbox" class="checkThree" name="text3" />
+                    <label class="pynctsTextThree" for="text3">
+                        Новые сообщения
+                    </label>
             </div>
-            <div class="box_four">
+            <div class="box_four" >
                 <UiCheckbox type="checkbox" class="checkFour" name="text4"/>
-                    <label class="pynctsTextFour" for="text4">Новые вакансии/соискатели</label>
+                    <label class="pynctsTextFour" for="text4">
+                        Новые вакансии/соискатели
+                    </label>
             </div>
         </div>
    </div>
    <div class="account" @click="account=!account" >
         <h1 class="title">Аккаунт</h1>
             <div class="arrow_icon">
-                <img class="arrow" src="~/shared/assets/images/arrow_back_ios.png">  
+                <img class="arrow" :class="{'turn-arrow': account}" src="~/shared/assets/images/arrow_back_ios.png">  
             </div>    
    </div>
-   <div class="bodyAccount" v-show="account">
-        <div v-for="(value, Phone) in object">
-            {{ Phone }}: {{ value }}
+   <div class="bodyAccount" v-show="account" style="width: 100%; height: 180px; margin-left: 80px;">
+        <div v-for="( Phone,value ) in object" style="padding-top: 30px; color: #604D9E; ">
+            <p style="display: inline-block;">{{ value }}</p>
+            <a style="display: inline-block; padding-left: 200px;">{{ Phone }}</a>            
         </div>
-        <button>Изменить пароль</button>
+        
+        <button style="color:#604D9E;padding-top: 30px; padding-right: 30px;">Изменить пароль</button>
     </div>
     <div class="Help" @click="help=!help" >
         <h1 class="title">Помощь</h1>
         <div class="arrow_icon">
-            <img class="arrow" src="~/shared/assets/images/arrow_back_ios.png">  
+            <img class="arrow" :class="{'turn-arrow': help}" src="~/shared/assets/images/arrow_back_ios.png">  
         </div>   
     </div>
    <div class="bodyHelp" v-show="help" style="margin-left:80px;">
@@ -80,6 +92,7 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
   export default defineComponent({
     name: 'SettingsPage',
     data: () => ({
+        checkboxString: 'Даю согласие на обработку своих <br> <router-link class="personal-data" to="/personal-data">персональных данных</router-link>',
         show:false,
         box:false,
         account:false,
@@ -87,13 +100,22 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
         
         
         object:{
-            Phone:'8950705##',
+            Phone:'8950705####',
             Email:'ilaBorisov@yahoo.com'
         },
     }),
     components:{ UiCheckbox, UiInput, UiTextarea, UiButton },
     props: {},
-    computed: {},
+    computed: {
+        arrowLeft() {
+        let arrow = 'turn-arrow';
+        if (this.show === true) {
+        return arrow;
+        }
+        return true;
+
+   },
+    },
     methods: {
     toggle: function() {
       this.show = !this.show;
@@ -102,6 +124,8 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
       this.help = !this.help;
     
     },
+
+    
     
     
     },
@@ -130,6 +154,12 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
         display: inline-block;
           
     };
+    .turn-arrow {
+    -moz-transform: rotate(180deg);
+    -ms-transform: rotate(180deg);
+    -webkit-transform: rotate(180deg);
+    transform: rotate(180deg);
+    };
 
    
 
@@ -154,6 +184,7 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
     };
     .white{
         display: inline-block;
+        
     };
     .whiteInput{
         margin-left: 180px;
@@ -165,13 +196,9 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
         color: #604D9E;
         display: inline-block;
         cursor: pointer;
-        
-    
-
 
     };
 
- 
 
     .darckInput{
         margin-left: 30px;
@@ -186,6 +213,9 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
         cursor: pointer;
     };
 
+
+
+
     .notifications{
         color: #604D9E;
         padding-left:40px ;
@@ -195,9 +225,10 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
 
     .pyncts{
         width: 100%;
-        display: block;
+        height: 200px;
         padding-top:20px;
         padding-left:70px;
+        
 
     };
 
@@ -205,24 +236,33 @@ import UiTextarea from 'widgets/ui/UiTextarea.vue';
     .pynctsTextTwo ,
     .pynctsTextThree ,
     .pynctsTextFour{
-        
+       display: block;
+       padding-left: 50px;
         color:#604D9E;
+        cursor: pointer;
     };
 
     .checkOne,
     .checkTwo,
     .checkThree,
     .checkFour{
-       display: block; 
-       
-
+        position:absolute;
+        padding-right:50px ;
+        display: block;
+        float: left;
+        width: 25px;
+        height: 25px;
     };
+
 
     .box_one,
     .box_two,
     .box_three,
     .box_four{
-        display: inline-block;
+        
+        margin-top:20px ;
+        width: auto;
+
     };
 
 
